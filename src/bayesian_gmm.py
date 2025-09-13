9c2823a8702edd1811dda619cec22936143aaa0f
gabriel <gabriel_jiglau@yahoo.com>
2025-09-13 22:13:11 +0300

    add priors to bayesian gmm

--- src/bayesian_gmm.py
+++ src/bayesian_gmm.py
@@ -1,16 +1,17 @@
 import numpy as np
 from scipy.stats import dirichlet
 from scipy.special import gammaln, psi  # psi is the logarithmic derivative of the gamma function
-from utils import dirichlet_pdf
+from utils import (init_cluster_means, init_precision_matrix, init_priors, init_soft_counts, dirichlet_pdf,
+                   compute_weighted_mean)
 
 class NormalInverseWishartDistribution:
 
     def __init__(self, strength_mean=None, mean=None, freedom_degrees=None, precision_matrix=None):
 
-        self._strength_mean = 1 if strength_mean is None else self._strength_mean = strength_mean
-        self._mean = 0 if mean is None else self._mean = mean
-        self._freedom_degrees = 0 if freedom_degrees is None else self._freedom_degrees = freedom_degrees
-        self._precision_matrix = [] if precision_matrix is None else self._precision_matrix = precision_matrix
+        self._strength_mean = strength_mean
+        self._mean = mean
+        self._freedom_degrees = freedom_degrees
+        self._precision_matrix = precision_matrix
 
     @property
     def strength_mean(self):
@@ -47,27 +48,64 @@ class NormalInverseWishartDistribution:
 
 class BayesianMixtureModel:
 
-    def __init__(self, no_clusters: int, mean_strength_prior, mean_prior, freedom_degree_prior, precision_matrix_prior):
+    def __init__(self, no_clusters: int):
+
         self.no_clusters = no_clusters
         self.weights = [(1 / no_clusters) for _ in range(no_clusters)]
-        self.weights_dist = dirichlet_pdf(self.weights)
+        self.weights_pdf = dirichlet_pdf(self.weights)
         self.responsibilities = []
-        self.priors = [NormalInverseWishartDistribution for _ in range(no_clusters)]
+        self.prior_hyperparameters = []
+        self.niw_posteriors = [NormalInverseWishartDistribution for _ in range(no_clusters)]
+
 
     def train(self, num_iterations, x_train, y_train=None):
 
-        # parameter initialization
         dim_data = x_train.shape[0]
-        self.responsibilities = np.array((self.no_clusters, dim_data))
-        for prior in self.priors:
-            prior.freedom_degrees = dim_data + 2
+
+        # parameter initialization
+        labels, cluster_means = init_cluster_means(x_train, self.no_clusters)
+        self.responsibilities = init_soft_counts(self.no_clusters, labels, x_train)
+        print(f"self.responsibilities = {self.responsibilities}")
+        weighted_means = compute_weighted_mean(x_train, labels, self.no_clusters, self.responsibilities)
+
+        # data_mean, strength_mean (the confidence in the data_mean), degree_of_freedom, precision_matrix
+        self.prior_hyperparameters = init_priors(x_train, labels, self.no_clusters)
+        precision_matrix = init_precision_matrix(x_train, labels, self.no_clusters)
+
+        for i, posterior in enumerate(self.niw_posteriors):
+            posterior.mean = cluster_means[i]
+            posterior.strength_mean = 1
+            posterior.freedom_degrees = dim_data + 2
+            posterior.precision_matrix = precision_matrix
 
         for i in range(num_iterations):
 
+            self.e_step(x_train)
+            self.variational_m_step(x_train, weighted_means)
 
-            pass
 
+    def e_step(self, x_train):
+        """
+        update the responsibilities (i.e. the probability that point x_i belongs to each cluster)
+        """
+        total_responsibility = np.sum(self.responsibilities[:, :])
+        print(f"total_responsibility = {total_responsibility} \n")
+        for cluster_idx in range(self.no_clusters):
+            for data_idx, row in enumerate(x_train.itertuples()):
+                # here the first term is the responsibility of the whole cluster
+                cluster_responsibility = np.sum(self.responsibilities[cluster_idx, :])
+                log_zi_prob = psi(cluster_responsibility) - psi(total_responsibility)
+                log_xi_prob = None
 
+        pass
+
+    def variational_m_step(self, x_train, weighted_means):
+        """
+        update the probabilities of the weights(from the dirichlet distribution),
+        as well as all the parameters of the NormalInverseWishart: strength_mean, degree_of_freedom, precision_matrix
+        """
+
+        pass
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.cluster import KMeans
 from scipy.stats import dirichlet
 
 def init_cov_matrix(dim: int, small: bool=True):
@@ -51,3 +52,72 @@ def relabel_data(num_clusters, cluster_indices, num_classes):
 
 def dirichlet_pdf(weights):
     return dirichlet(weights)
+
+def init_cluster_means(x_train, no_clusters):
+
+    k_means = KMeans(n_clusters=no_clusters, n_init=1, max_iter=1, random_state=13)
+    labels = k_means.fit_predict(x_train)
+
+    return labels, k_means.cluster_centers_
+
+def init_soft_counts(no_clusters, labels, x_train):
+
+    """
+    simulate the soft counts, since we used k-means (hard clustering algorithm) to initialize the clusters
+    """
+
+    additive = 1.4 / no_clusters
+    soft_counts = np.full((no_clusters, x_train.shape[0]), additive)
+    for index, x_row in enumerate(x_train.itertuples()):
+        soft_counts[labels[index], index] += 1
+
+    soft_counts /= ((additive * no_clusters) + 1) # normalization
+    return soft_counts
+
+
+def compute_weighted_mean(x_train, labels, no_clusters, soft_counts):
+
+    print(f"type(soft_counts) = {type(soft_counts)}")  ## de ce primesc int aici ?
+
+    weighted_means = np.zeros((no_clusters, x_train.shape[1]))
+
+    for i, row in enumerate(x_train.itertuples()):
+        current_cluster = labels[i]
+        weighted_means[current_cluster,:] += np.array(row[1:]) * soft_counts[current_cluster, i]
+
+    return weighted_means
+
+
+def init_precision_matrix(x_train, labels, no_clusters):
+
+    weighted_means = compute_weighted_mean(x_train, labels, no_clusters, x_train.shape[0])
+    covariance_matrix = 0
+    for i, x_row in enumerate(x_train.itertuples()):
+        data_diff =  weighted_means[labels[i]] / x_row[1:]
+        data_diff = data_diff.reshape(-1, 1)
+        covariance_matrix += data_diff @ data_diff.transpose()
+
+    # print(f"cov_matrix = {covariance_matrix}")
+    covariance_matrix /= x_train.shape[0]
+    precision_matrix = np.linalg.inv(covariance_matrix)
+    # print(np.all(np.linalg.eigvals(precision_matrix) > 0)) # sanity check, all eigenvalues should be positive
+    return np.linalg.inv(covariance_matrix)
+
+
+## aici se plange
+def init_priors(x_train, labels, no_clusters):
+
+    priors, data_mean = [], []
+    cols = x_train.columns[:-1]
+    for i in range(len(cols)):
+        dimension_mean = x_train[cols[i]].mean()
+        data_mean.append(dimension_mean)
+
+    priors.append(data_mean)
+    priors.append(1)  # the confidence in our means; 1 ~ 'not so much'
+    priors.append(x_train.shape[1] + 2)  # degree of freedom
+    priors.append(init_precision_matrix(x_train, labels, no_clusters))
+
+    return priors
+
+
