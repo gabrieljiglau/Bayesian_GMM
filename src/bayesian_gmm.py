fix numerical instability in precision matrix

src/bayesian_gmm.py
@@ -1,13 +1,12 @@
 import numpy as np
 from scipy.stats import dirichlet
 from scipy.special import gammaln, psi  # psi is the logarithmic derivative of the gamma function
-from utils import init_cluster_means, init_priors, init_responsibilities
+from utils import init_cluster_means, init_priors, init_responsibilities, log_det_cholesky
 
 
 class NormalInverseWishartDistribution:
 
     def __init__(self, strength_mean=None, mean=None, freedom_degrees=None, precision_matrix=None):
-
         self._strength_mean = strength_mean
         self._cluster_mean = mean
         self._freedom_degrees = freedom_degrees
@@ -48,40 +47,38 @@ class NormalInverseWishartDistribution:
 
 class BayesianMixtureModel:
 
-    def __init__(self, no_clusters: int):
+    def __init__(self, no_clusters: int, epsilon=1e-14):
 
         self.no_clusters = no_clusters
         self.weights = [(1 / no_clusters) for _ in range(no_clusters)]
         self.prior_hyperparameters = []
         self.responsibilities = []
-        self.niw_posteriors = [NormalInverseWishartDistribution for _ in range(no_clusters)]
+        self.niw_posteriors = [NormalInverseWishartDistribution() for _ in range(no_clusters)]
+        self.epsilon = epsilon
 
 
-    def train(self, num_iterations, x_train, y_train=None):
+    def train(self, num_iterations, x_train, y_train=None, eps=1e-6):
 
         # parameter initialization
         dim_data = x_train.shape[1]
         labels, cluster_means = init_cluster_means(x_train, self.no_clusters)
 
         self.responsibilities = init_responsibilities(self.no_clusters, labels, x_train)
-        # weighted_mean_0, strength_mean (the confidence in the data_mean), degree_of_freedom, precision_matrix
+        # priors: data_mean, strength_mean (the confidence in the data_mean), degree_of_freedom, precision_matrix
         self.prior_hyperparameters = init_priors(x_train, labels, self.no_clusters, self.responsibilities)
 
         for i, posterior in enumerate(self.niw_posteriors):
             posterior.cluster_mean = cluster_means[i]
             posterior.strength_mean = self.prior_hyperparameters[2] + 1
             posterior.freedom_degrees = dim_data + 2
-            posterior.precision_matrix = self.prior_hyperparameters[3]
-
-        # !!! tre' sa stii ce faci de la bun inceput, transformi sau nu x_train in numpy array sau il lasi dataframe ?
+            posterior.precision_matrix = self.prior_hyperparameters[3][i] + np.eye(dim_data) * eps
 
         for i in range(num_iterations):
-            print(f"Now at iteration {i} \n")
+            print(f"Now at iteration {i}")
 
             self.e_step(x_train, dim_data, cluster_means)
             self.variational_m_step(x_train)
             print(f"self.weights = {self.weights}")
-            print(f"self.weighted_means = {self.prior_hyperparameters[0]}")
 
 
     def predict(self, x_test, y_test):
@@ -92,21 +89,23 @@ class BayesianMixtureModel:
 
         first_term = 0
         second_term = 0
-        for dimension in range(1, no_features):
+        for dimension in range(no_features):
             first_term += psi((cluster_responsibility + 1 - dimension) / 2)
 
-        first_term += no_features * np.log(2) + np.log(np.linalg.det(self.niw_posteriors[cluster_idx].precision_matrix))
+        log_det = log_det_cholesky(self.niw_posteriors[cluster_idx].precision_matrix)
+        first_term += no_features * np.log(2) + log_det
         first_term /= 2
         first_term -= (no_features / 2) * np.log(np.pi * 2)
 
         diff = data_in - cluster_mean
         diff = diff.reshape(-1, 1)
 
-        second_term += (diff.transpose() @ self.niw_posteriors[cluster_idx].precision_matrix) @ diff
+        second_term += diff.transpose() @ self.niw_posteriors[cluster_idx].precision_matrix @ diff
         second_term *= self.niw_posteriors[cluster_idx].freedom_degrees
         second_term += no_features / self.niw_posteriors[cluster_idx].strength_mean
+        second_term /= 2
 
-        return first_term - second_term / 2
+        return first_term - second_term
 
 
     def e_step(self, x_train, no_features, cluster_means):
@@ -115,32 +114,31 @@ class BayesianMixtureModel:
         """
         total_weight = np.sum(self.weights)
         print(f"total_weight = {total_weight} \n")
-        # print(f"inainte de for self.responsibilities = {self.responsibilities}")
+        print(f"inainte de for self.responsibilities = {self.responsibilities}")
         for k in range(self.no_clusters):
             for data_idx, row in enumerate(x_train.itertuples()):
-                # TODO: debugging AICI, ceva se intampla cu cluster_weight
                 cluster_weight = self.weights[k]
                 # print(f"cluster_weight = {cluster_weight}")
                 log_zi_prob = psi(cluster_weight) - psi(total_weight)
                 log_xi_prob = self.compute_log_xi(k, cluster_weight, cluster_means[k], row[1:], no_features)
                 # print(f"log_zi_prob = {log_zi_prob}")
                 # print(f"log_xi_prob = {log_xi_prob}")
-                self.responsibilities[k, data_idx] = log_zi_prob + log_xi_prob
+                responsibility = log_zi_prob + log_xi_prob
+                # if responsibility < self.epsilon:
+                    # responsibility = 0
+                self.responsibilities[k, data_idx] = responsibility
 
-        # print(f"dupa for self.responsibilities = {self.responsibilities}")
-        # subtracting the max to prevent overflows
-        for k in range(self.no_clusters):
-            max_prob = np.argmax(self.responsibilities[k, :])
-            self.responsibilities[k, :] -= max_prob
+        print(f"dupa for self.responsibilities = {self.responsibilities}")
+        # normalization and subtracting the max to prevent overflows
 
+        ## TODO: aici sa exponentieti diferenta, nu totul
+        self.responsibilities -= self.responsibilities.max(axis=1, keepdims=True)
         self.responsibilities = np.exp(self.responsibilities)
+        self.responsibilities /= self.responsibilities.sum(axis=1, keepdims=True)
 
-        # normalization
-        for k in range(self.no_clusters):
-            normalization_term = np.sum(np.exp(self.responsibilities[k, :]))
-            for data_idx in range(x_train.shape[0]):
-                self.responsibilities[k, data_idx] = np.exp(self.responsibilities[k, data_idx]) / normalization_term
+        print(f"self.responsibilities.sum(axis=1) = {self.responsibilities.sum(axis=1)}")
 
+        print(f"after normalization: {self.responsibilities}")
 
     def build_sample_covariance(self, x_train, soft_counts):
 
@@ -148,21 +146,21 @@ class BayesianMixtureModel:
         for k in range(self.no_clusters):
             cov_dim = 0
             for idx, row in enumerate(x_train.itertuples()):
-                diff = np.array(row[1:]) - self.means[k]  ## aici formula e gresita; ar trebui sa fie weighted_mean - prev_mean
+                diff = np.array(row[1:]) - self.prior_hyperparameters[0] # the data_mean
                 diff = diff.reshape(-1, 1)
                 cov_dim += (diff @ diff.transpose()) * self.responsibilities[k, idx]
 
-            print(f"cov_dim = {cov_dim}")
-            print(f"soft_counts[k] = {soft_counts[k]}")  # de ce toate soft_count-urile sunt 0.99 ??
+            # print(f"cov_dim = {cov_dim}")
+            # print(f"soft_counts[k] = {soft_counts[k]}")  # should sumt up to 1
             sample_covariance.append(cov_dim / soft_counts[k])
 
         return np.array(sample_covariance)
 
 
-    def build_coefficient(self, k, soft_count, old_mean):
+    def build_coefficient(self, k, soft_count, data_mean):
 
         first_term = ((self.niw_posteriors[k].strength_mean - soft_count) * soft_count) / self.niw_posteriors[k].strength_mean
-        diff = self.niw_posteriors[k].cluster_mean - old_mean
+        diff = self.niw_posteriors[k].cluster_mean - data_mean
         diff = diff.reshape(-1, 1)
 
         return (diff @ diff.transpose()) * first_term
@@ -184,9 +182,7 @@ class BayesianMixtureModel:
         for k in range(self.no_clusters):
             self.weights[k] += soft_counts[k]
 
-        print(f"soft_counts = {soft_counts} \n ")
-
-        # !! ALL THE CLUSTERS HAVE THE SAME MEAN AS THE CENTER ????
+        # print(f"soft_counts = {soft_counts} \n ")
 
         # new weighted mean
         weighted_means = np.zeros((self.no_clusters, x_train.shape[1]))
@@ -203,16 +199,13 @@ class BayesianMixtureModel:
             self.niw_posteriors[k].strength_mean += soft_counts[k]
             self.niw_posteriors[k].freedom_degrees += soft_counts[k]
 
-            old_mean = self.niw_posteriors[k].cluster_mean
-            mean_contribution = (self.niw_posteriors[k].strength_mean - soft_counts[k]) * old_mean
-            data_contribution = soft_counts[k] * weighted_means[k, :]
-
-            self.niw_posteriors[k].cluster_mean = (mean_contribution + data_contribution) / self.niw_posteriors[k].strength_mean
+            mean_contribution = (self.niw_posteriors[k].strength_mean - soft_counts[k]) * self.prior_hyperparameters[0]
+            empirical_contribution = soft_counts[k] * weighted_means[k, :]
+            self.niw_posteriors[k].cluster_mean = (mean_contribution + empirical_contribution) / self.niw_posteriors[k].strength_mean
 
             observed_data = soft_counts[k] * sample_covariance[k]
-            uncertainty_coefficient = self.build_coefficient(k, soft_counts[k], old_mean)
-            self.niw_posteriors[k].precision_matrix = (self.niw_posteriors[k].precision_matrix + observed_data +
-                                                       uncertainty_coefficient)
+            uncertainty_coefficient = self.build_coefficient(k, soft_counts[k], self.prior_hyperparameters[0])
+            self.niw_posteriors[k].precision_matrix = self.niw_posteriors[k].precision_matrix + observed_data + uncertainty_coefficient
 
 
 

src/utils.py
@@ -59,6 +59,11 @@ def init_cluster_means(x_train, no_clusters):
 
     return labels, k_means.cluster_centers_
 
+def log_det_cholesky(matrix):
+
+    cholesky_det = np.linalg.cholesky(matrix)
+    return np.sum(np.log(np.diag(cholesky_det))) * 2
+
 def init_responsibilities(no_clusters, labels, x_train):
 
     """
@@ -71,10 +76,10 @@ def init_responsibilities(no_clusters, labels, x_train):
         soft_counts[labels[index], index] += 1
 
     soft_counts /= ((additive * no_clusters) + 1) # normalization
-    return soft_counts
+    return np.array(soft_counts)
 
 
-def weighted_mean_m_step(x_train, no_clusters, soft_counts):
+def _weighted_mean(x_train, no_clusters, soft_counts):
 
     weighted_means = np.zeros((no_clusters, x_train.shape[1]))
     for k in range(no_clusters):
@@ -88,28 +93,27 @@ def weighted_mean_m_step(x_train, no_clusters, soft_counts):
 
 def init_precision_matrix(x_train, labels, no_clusters, soft_counts):
 
-    weighted_means = weighted_mean_m_step(x_train, no_clusters, soft_counts)
-    covariance_matrix = 0
-    for i, x_row in enumerate(x_train.itertuples()):
-        data_diff =  weighted_means[labels[i]] / x_row[1:]
-        data_diff = data_diff.reshape(-1, 1)
-        covariance_matrix += data_diff @ data_diff.transpose()
+    weighted_means = _weighted_mean(x_train, no_clusters, soft_counts)
+    covariance_matrices = []
+    for k in range(no_clusters):
+        covariance_matrix = 0
+        for data_idx, x_row in enumerate(x_train.itertuples()):
+            data_diff = x_row[1:] - weighted_means[labels[data_idx]]
+            data_diff = data_diff.reshape(-1, 1)
+            covariance_matrix += (data_diff @ data_diff.transpose()) * soft_counts[k][data_idx]
+        covariance_matrices.append(covariance_matrix / x_train.shape[0])
 
-    # print(f"cov_matrix = {covariance_matrix}")
-    covariance_matrix /= x_train.shape[0]
-    precision_matrix = np.linalg.inv(covariance_matrix)
-    # print(np.all(np.linalg.eigvals(precision_matrix) > 0)) # sanity check, all eigenvalues should be positive
-    return precision_matrix
+    return [np.linalg.inv(cov_matrix) for cov_matrix in covariance_matrices]
 
-def get_data_mean(x_train):
+def _data_mean(x_train):
 
-    data_mean = []
+    data_mean = np.zeros(x_train.shape[1])
     for i in range(x_train.shape[1]):
         dimension_mean = x_train.iloc[:, i].mean()
-        data_mean.append(dimension_mean)
+        data_mean[i] = dimension_mean
     return data_mean
 
 def init_priors(x_train, labels, no_clusters, soft_counts):
 
-    return [get_data_mean(x_train), 1, x_train.shape[1] + 2, init_precision_matrix(x_train, labels, no_clusters, soft_counts)]
+    return [_data_mean(x_train), 1, x_train.shape[1] + 2, init_precision_matrix(x_train, labels, no_clusters, soft_counts)]
 